0635ac5a6dfa3d79ee1ed48b8bdf29b7ba383c6c
Taehyun Hwang <taehyun95@gmail.com>
2017-09-07 18:15:54 +0900

    [ScrollView] Implement DefaultScrollView

---
+++ Assets/ScrollView/Scripts/DefaultScrollView.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+[DisallowMultipleComponent]
+[RequireComponent(typeof(ScrollRect))]
+public class DefaultScrollView : MonoBehaviour
+{
+    public delegate RectTransform GetItem(int index);
+    [HideInInspector]
+    public GetItem _delegateGetItem = null;
+    [HideInInspector]
+    public int _size = 0;
+
+    public void Refresh()
+    {
+        if (!gameObject.activeSelf)
+        {
+            return;
+        }
+        if (_delegateGetItem == null)
+        {
+            Debug.LogError("GetItem method is null!");
+        }
+
+        // clear content
+        var content = GetComponent<ScrollRect>().content;
+        for (int i = content.childCount - 1; i >= 0; i--)
+        {
+            Destroy(content.GetChild(i).gameObject);
+        }
+
+        // add item
+        for (int i = 0; i < _size; i++)
+        {
+            // get item
+            var item = _delegateGetItem(i);
+            if (item == null)
+            {
+                Debug.LogWarning("item index - " + i.ToString() + " - is null");
+                continue;
+            }
+
+            // set item to content's child
+            item.SetParent(content);
+            item.localPosition = Vector3.zero;
+            item.localScale = Vector3.one;
+        }
+
+        // init position
+        content.localPosition = Vector3.zero;
+    }
+}
---
+++ Assets/ScrollView/Scripts/ScrollViewSceneManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollViewSceneManager : MonoBehaviour
+{
+    public GameObject _scrollRect;
+    public GameObject _scrollRect2;
+    public GameObject _scrollRect3;
+    public GameObject _item;
+    public int _size = 10;
+
+    private void Awake()
+    {
+        // horizontal view
+        _scrollRect.GetComponent<DefaultScrollView>()._size = _size;
+        _scrollRect.GetComponent<DefaultScrollView>()._delegateGetItem = GetItem;
+        _scrollRect.GetComponent<DefaultScrollView>().Refresh();
+
+        // vertical view
+        _scrollRect2.GetComponent<DefaultScrollView>()._size = _size;
+        _scrollRect2.GetComponent<DefaultScrollView>()._delegateGetItem = GetItem;
+        _scrollRect2.GetComponent<DefaultScrollView>().Refresh();
+
+        // grid view
+        _scrollRect3.GetComponent<DefaultScrollView>()._size = _size;
+        _scrollRect3.GetComponent<DefaultScrollView>()._delegateGetItem = GetItem;
+        _scrollRect3.GetComponent<DefaultScrollView>().Refresh();
+    }
+
+    public RectTransform GetItem(int index)
+    {
+        if (index < 0 || index >= _size)
+        {
+            return null;
+        }
+        var item = Instantiate(_item) as GameObject;
+        item.GetComponentInChildren<Text>().text = (index + 1).ToString();
+        return item.GetComponent<RectTransform>();
+    }
+}
